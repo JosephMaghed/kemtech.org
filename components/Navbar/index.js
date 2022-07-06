c6060d3e7aff9f491b37b427009e2eb26b575505
Omar Sakr <o.sakr@ptscorp-eg.com>
2022-07-07 00:38:21 +0200

    feat: add navBar

---
+++ components/Navbar/index.js
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../../assets/kemtech_full_white.svg";
+import PrimaryButton from "../PrimaryButton";
+import styles from "./Navbar.module.sass";
+
+export default function Navbar() {
+  let navLinks = [
+    { pageName: "About kemtech", pageLink: "/about" },
+    { pageName: "Initiative programs", pageLink: "/programs" },
+    { pageName: "Community", pageLink: "/community" },
+    { pageName: "Careers", pageLink: "/careers" },
+    { pageName: "Partners", pageLink: "/partners" },
+  ];
+
+  return (
+    <nav className={styles.navbar}>
+      <Link href="/">
+        <Image
+          className={styles.logo}
+          src={logo}
+          width={100}
+          height={100}
+          alt="Kemtech logo"
+        />
+      </Link>
+      <div className={styles.navContainer}>
+        {navLinks.map((navLink) => (
+          <div key={navLink.pageLink} className={styles.navItem}>
+            <Link href={navLink.pageLink}>{navLink.pageName}</Link>
+          </div>
+        ))}
+      </div>
+      <PrimaryButton innerText="Apply now" />
+    </nav>
+  );
+}
