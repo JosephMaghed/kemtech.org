c615256f8624b0c9d8cadb0d8822eda8410cfb68
Omar Sakr <o.sakr@ptscorp-eg.com>
2022-07-16 02:59:46 +0200

    feat: nav bar responsive for small screens

--- src/components/Layout/Navbar/Navbar.js
+++ src/components/Layout/Navbar/Navbar.js
@@ -1,14 +1,33 @@
 import Image from "next/image";
 import Link from "next/link";
-import logo from "assets/kemtech_full_white.svg";
 import Button from "components/Button";
-import styles from "./Navbar.module.sass";
 import { useRouter } from "next/router";
+import { useState } from "react";
+
+//styles
+import styles from "./Navbar.module.sass";
+
+//imgs
+import logo from "assets/kemtech_full_white.svg";
+import hamburgerMenu from "assets/menu-icon.svg";
+import xIcon from "assets/x.svg";
 
 export default function Navbar() {
   // Current page
   const router = useRouter();
   const currentPage = router.pathname;
+
+  //responsive nav toggle handler
+  const [isToggled, setToggled] = useState(false);
+
+  const navToggleHandler = (isToggleBoolean) => {
+    setToggled(() => isToggleBoolean || !isToggled, [isToggled, setToggled]);
+
+    // Turn off scroll when toggled
+    !isToggled
+      ? (document.body.style.overflow = "hidden")
+      : (document.body.style.overflow = "visible");
+  };
 
   // Data to Generate nav links
   let navLinks = [
@@ -21,31 +40,44 @@ export default function Navbar() {
 
   return (
     <nav className={styles.navbar}>
-      <Link href="/">
-        <Image
-          className={styles.logo}
-          src={logo}
-          width={100}
-          height={100}
-          alt="Kemtech logo"
-        />
-      </Link>
-      <div className={styles.navContainer}>
-        {navLinks.map((navLink) => (
-          <div
-            key={navLink.pageLink}
-            className={
-              currentPage === navLink.pageLink
-                ? styles.navItemActive
-                : styles.navItem
-            }
-          >
-            <Link href={navLink.pageLink}>{navLink.pageName}</Link>
-          </div>
-        ))}
+      <div
+        className={isToggled ? styles.navWarperActive : styles.navWarper}
+        onClick={() => navToggleHandler(false)}
+      >
+        <Link href="/">
+          <Image
+            className={styles.logo}
+            src={logo}
+            width={100}
+            height={100}
+            alt="Kemtech logo"
+          />
+        </Link>
+        <div className={styles.navItemContainer}>
+          {navLinks.map((navLink) => (
+            <div
+              key={navLink.pageLink}
+              className={
+                currentPage === navLink.pageLink
+                  ? styles.navItemActive
+                  : styles.navItem
+              }
+              onClick={() => navToggleHandler(false)}
+            >
+              <Link href={navLink.pageLink}>{navLink.pageName}</Link>
+            </div>
+          ))}
+        </div>
+        <div className={styles.applyButton}>
+          <Button innerText="Apply now" type="primary" />
+        </div>
+        <div className={styles.xToggle} onClick={() => navToggleHandler(false)}>
+          <Image src={xIcon} alt="" height={50} />
+        </div>
       </div>
-      <div className={styles.applyButton}>
-        <Button innerText="Apply now" type="primary" />
+
+      <div className={styles.menuToggle} onClick={() => navToggleHandler(true)}>
+        <Image src={hamburgerMenu} alt="" height={30} width={40} />
       </div>
     </nav>
   );
