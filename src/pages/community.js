a9b52f61f710e8096a4d3f398076782bfa447a91
Omar Sakr <o.sakr@ptscorp-eg.com>
2022-07-22 18:07:00 +0200

    fix: add title to community page

--- src/pages/community.js
+++ src/pages/community.js
@@ -1,9 +1,14 @@
 import NotReady from "components/NotReady";
 import pageArt from "assets/lunch-break-two-color.svg";
+import Head from "next/head";
 
-export default function careers() {
+export default function community() {
   return (
     <>
+      <Head>
+        <title>Community - Kemtech</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <NotReady img={pageArt}>
         <strong>Hello Kemtechians,</strong>
         <br /> Stay tuned we are building our amazing technovative community.
