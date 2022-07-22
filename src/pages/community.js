736b31955e69630c4f465cd4f8b0707f532392db
Omar Sakr <o.sakr@ptscorp-eg.com>
2022-07-22 17:09:52 +0200

    feat: add comunity page

---
+++ src/pages/community.js
@@ -0,0 +1,13 @@
+import NotReady from "components/NotReady";
+import pageArt from "assets/lunch-break-two-color.svg";
+
+export default function careers() {
+  return (
+    <>
+      <NotReady img={pageArt}>
+        <strong>Hello Kemtechians,</strong>
+        <br /> Stay tuned we are building our amazing technovative community.
+      </NotReady>
+    </>
+  );
+}
