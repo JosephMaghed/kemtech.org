067326ad0b0b732f3707d5471837dca996de0f50
Omar Sakr <o.sakr@ptscorp-eg.com>
2022-10-24 14:24:32 +0200

    Update Schedule

--- src/components/summit-page/SummitSchedule/scheduleData.js
+++ src/components/summit-page/SummitSchedule/scheduleData.js
@@ -1,119 +1,124 @@
 export const dayOneData = [
   {
-    startTime: "09:30 AM",
-    endTime: "10:30 AM",
+    startTime: "09:00 AM",
+    endTime: "10:00 AM",
     activityTitle: "Reception and Welcoming Ceremony for Ministers and Ambassadors",
     description: "(Closed Meeting)",
   },
   {
-    startTime: "10:30 AM",
-    endTime: "11:30 AM",
+    startTime: "10:00 AM",
+    endTime: "10:30 AM",
     activityTitle: "Opening of Kemtech Studio, the Egyptian Applied Technology Valley (Field Visit)",
     description: "Eman Wadie, CEO, ORO Ventures Lab",
   },
   {
-    startTime: "00:11",
-    endTime: "00:22",
+    startTime: "10:30 AM",
+    endTime: "10:40 AM",
     activityTitle: "Opening Speech",
     description: "Prof. Hesham El-Deeb, President of New Cairo Technological University (NCTU)",
   },
   {
-    startTime: "00:11",
-    endTime: "00:22",
+    startTime: "10:40 AM",
+    endTime: "10:50 AM",
     activityTitle: "Speech of the Official Partner",
     description: "Eng. Maha Mandour, Managing Director of PTS Investments Holdings Inc.",
   },
   {
-    startTime: "00:11",
-    endTime: "00:22",
+    startTime: "10:50 AM",
+    endTime: "11:00 AM",
     activityTitle: "Speech of the Chinese Ambassador to Egypt",
     description: "H. E. Mr. Liao Liqiang",
   },
   {
-    startTime: "00:11",
-    endTime: "00:22",
+    startTime: "11:00 AM",
+    endTime: "11:15 AM",
     activityTitle: "Signing Ceremony of Cooperation Protocols between Egyptian and Chinese Counterparts",
     description: "- Signing a Cooperation Protocol between New Cairo Technological University and Shenzhen University \n -  Signing a Cooperation Protocol between New Cairo Technological University and SIMIC-IN SPRING \n - Signing a Cooperation Protocol between the Egyptian Applied Technology Valley and Shanghai Silicon Valley",
   },
   {
-    startTime: "00:11",
-    endTime: "00:22",
+    startTime: "11:15 AM",
+    endTime: "11:25 AM",
     activityTitle: "Speech of the Initiative’s Strategic Partner for “Investment Cluster Program Cheque Handing Over Ceremony to Qualified Startups from Kemtech Studio",
     description: "Eng. Tarek Mohamed, Chairman of the Board of Directors of ORO Capital",
   },
   {
-    startTime: "00:11",
-    endTime: "00:22",
+    startTime: "11:25 AM",
+    endTime: "11:30 AM",
     activityTitle: "Speech of Diamond Sponsor",
   },
   {
-    startTime: "00:11",
-    endTime: "00:22",
+    startTime: "11:30 AM",
+    endTime: "11:45 AM",
     activityTitle: "A word from University Partners Presenting Joint Projects",
   },
   {
-    startTime: "00:11",
-    endTime: "00:22",
+    startTime: "11:45 AM",
+    endTime: "12:15 PM",
     activityTitle: "Main Speeches by Honorable Ministers",
     description: "- H. E. Eng. Mohamed Salah El Din, Minister of State for Military Production \n - H. E. Eng.  Ahmed Samir, Minister of Trade and Industry \n- H.E. Prof. Mohamed Ayman Ashour, Minister of Higher Education and Scientific Research \n"
   },
   {
-    startTime: "00:11",
-    endTime: "00:22",
+    startTime: "12:15 PM",
+    endTime: "12:35 PM",
     activityTitle: "Graduation Ceremony of the First Class of Higher Technological Diploma Students from Technological Universities",
   },
   {
-    startTime: "00:11",
-    endTime: "00:22",
+    startTime: "12:35 PM",
+    endTime: "01:00 PM",
     activityTitle: "Opening Ceremony of the Career Fair and the Associated Exhibition (Field Visit)",
     description: "Eng. Mohamed Sery, Chairman of the Board of Directors of Mentors’ Group "
   },
   {
-    startTime: "00:11",
-    endTime: "00:22",
-    activityTitle: "Speech on Metaverse Technology: Opportunities and Challenges",
-    description : "Eng. Medhat El-Husseini, Advisory Member of the Egyptian Applied Technology Valley"
+    startTime: "01:00 PM",
+    endTime: "01:30 PM",
+    activityTitle: "Break",
   },
   {
-    startTime: "00:11",
-    endTime: "00:22",
+    startTime: "01:30 PM",
+    endTime: "01:40 PM",
     activityTitle: "Speech of the Initiative’s Main Partner for Training, Career Development and Entrepreneurship Programs",
     description: "Eng. Mohamed Sery, Chairman of the Board of Directors of Mentors’ Group"
   },
   {
-    startTime: "00:11",
-    endTime: "00:22",
+    startTime: "01:40 PM",
+    endTime: "02:00 PM",
+    activityTitle: "Speech on Metaverse Technology: Opportunities and Challenges",
+    description : "Eng. Medhat El-Husseini, Advisory Member of the Egyptian Applied Technology Valley"
+  },
+  {
+    startTime: "02:00 PM",
+    endTime: "03:00 PM",
     activityTitle: "Panel Discussion 1:  The Role of Egyptian Technological Universities and Schools in Connecting Education to Labor Market",
   },
   {
-    startTime: "00:11",
-    endTime: "00:22",
+    startTime: "03:00 PM",
+    endTime: "03:30 PM",
     activityTitle: "Break",
   },
   {
-    startTime: "00:11",
-    endTime: "00:22",
+    startTime: "03:30 PM",
+    endTime: "04:30 PM",
     activityTitle: "Panel Discussion 2: The Role of Applied Technology in Supporting Sustainable Development in Africa",
   },
   {
-    startTime: "00:11",
-    endTime: "00:22",
+    startTime: "4:30 PM",
+    endTime: "05:00 PM",
     activityTitle: "Closing and Way Forward",
   },
 ];
 
 export const dayTwoData = [
   {
-    startTime: "00:11",
-    endTime: "00:22",
+    startTime: "09:00 AM",
+    endTime: "05:00 PM",
     activityTitle: "Activities of the Second Day of the Career Fair",
   },
 ];
 
 export const dayThreeData = [
   {
-    startTime: "00:11",
-    endTime: "00:22",
+    startTime: "09:00 AM",
+    endTime: "05:00 PM",
     activityTitle: "Activities of the Third Day of the Career Fair",
   },
 ];
