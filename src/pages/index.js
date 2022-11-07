dadc307349df7e87d09924f72201b000d5c0c629
Omar Sakr <o.sakr@ptscorp-eg.com>
2022-11-07 16:36:28 +0200

    remove old courses

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
       <ApplyProcess />
       {/* <ProgramsView /> */}
       <ImagesCarousel />
-      <OnGoingActivities />
+      {/* <OnGoingActivities /> */}
 
       <InitiativeGoals />
       <ContactForm />
