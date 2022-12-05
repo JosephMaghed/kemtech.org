be1acd143c5f1526a545f0edfa1af78be0a9d430
Omar Sakr <o.sakr@ptscorp-eg.com>
2022-12-05 14:41:43 +0200

    Add privacy policy page

---
+++ src/pages/privacy-policy.js
@@ -0,0 +1,96 @@
+export default function PrivacyPolicy() {
+	return (
+		<div className="container py-10 lg:w-3/4">
+			<h1 className="text-3xl md:text-4xl font-bold text-blue-basic">
+				Privacy Policy
+			</h1>
+			<p className="mt-4">
+				Kemtech Valley is committed to ensuring that your privacy is
+				protected. This privacy notice will inform you as to how we look
+				after your data and inform you about your privacy rights and how
+				the law protects you. You have the right to access, rectify,
+				cancel and oppose your data by writing to Kemtech Valley or by
+				e-mail or to{" "}
+				<a
+					href="mailto:info@kemtech.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<span className="underline pointer">Info@kemtech.org</span>
+				</a>
+			</p>
+
+			<h3 className="text-lg font-semibold text-blue-400 mt-8">
+				INFORMATION WE COLLECT FROM YOU
+			</h3>
+			<p className="mt-2">
+				If you contact us by telephone, email or by using the contact
+				form on this website we may collect the following information:
+				<br /> <br />
+				Your identity including: first name, last name, title, gender,
+				email, Phone and other information you have provided to contact
+				you. Transaction data: information about your usage of our
+				website or products and services that may be of interest based
+				on past correspondence. We use and analyze this information we
+				collect so that we can administer, support, improve and expand
+				our business. We may periodically send promotional emails or
+				updates relating to new services which we feel may be of
+				interest to you.
+				<br />
+				We do not collect any special person information about you (this
+				includes religious or philosophical beliefs, race or ethnic
+				origin, trade union membership, political opinions, health, DNA,
+				or criminal behavior).
+			</p>
+
+			<h3 className="text-lg font-semibold text-blue-400 mt-8">
+				HOW WE MAY USE YOUR INFORMATION
+			</h3>
+			<p className="mt-2">
+				To process any enrollment you have made, either by email,
+				telephone or online in order to comply with our obligations. To
+				respond to any correspondence, enquiries or requests. To prevent
+				fraud and report any illegal activities. We may use your
+				information to contact you for marketing updates or research. We
+				may contact you by email, phone or post. To send you feedback or
+				survey requests to help improve our services. We do this as a
+				part of our legitimate interest to help make our products and
+				services more relevant to you. We may send you promotional
+				materials informing you about the new development, special
+				offers, promotions company updates. You can opt out of this
+				service any time.
+			</p>
+
+			<h3 className="text-lg font-semibold text-blue-400 mt-8">
+				SECURITY AND DATA RETENTION
+			</h3>
+			<p className="mt-2">
+				We are committed to ensuring that your information is safe and
+				secure from unauthorized persons and against unlawful
+				processing, accidental loss, destruction and damage. We retain
+				your information for a reasonable and lawful period. Our site is
+				secured by SSL encryption to protect any personal data provided.
+			</p>
+
+			<h3 className="text-lg font-semibold text-blue-400 mt-8">
+				WHAT ARE COOKIES AND WHY DO WE USE THEM?
+			</h3>
+			<p className="mt-2">
+				A cookie is a small file which asks permission to be placed on
+				your computer’s hard drive. Once you agree, the file is added
+				and the cookie helps us analyze web traffic.
+			</p>
+
+			<h3 className="text-lg font-semibold text-blue-400 mt-8">
+				CONTROLLING YOUR INFORMATION
+			</h3>
+			<p className="mt-2">
+				We will not sell, distribute or lease your personal information
+				to third parties. You may request details of information held
+				about you and you may ask us to make necessary changes to ensure
+				that it is accurate, kept up to date or removed if there is no
+				legitimate reason for keeping it.
+			</p>
+		</div>
+	);
+}
