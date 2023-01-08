dcd2c97428dbd6a1d7ebf43d7ad44490db454415
Omar Sakr <o.sakr@ptscorp-eg.com>
2023-01-09 01:37:21 +0200

    Styles: center the contact form on contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,17 +2,22 @@ import ContactForm from "components/ContactForm";
 import Head from "next/head";
 
 export default function contact() {
-  return (
-    <>
-      <Head>
-        <title>Contact Us</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          name="description"
-          content="Kemtech Valley, NCT university, Main building, El lotus compound, 5th settlement, New Cairo, Egypt."
-        />
-      </Head>
-      <ContactForm />;
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>Contact Us</title>
+				<meta
+					name="viewport"
+					content="initial-scale=1.0, width=device-width"
+				/>
+				<meta
+					name="description"
+					content="Kemtech Valley, NCT university, Main building, El lotus compound, 5th settlement, New Cairo, Egypt."
+				/>
+			</Head>
+			<div className="container mx-auto my-20 md:w-3/4">
+				<ContactForm />
+			</div>
+		</>
+	);
 }
