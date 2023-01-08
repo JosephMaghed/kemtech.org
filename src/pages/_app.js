e0d5308a72142b7d916e9d534aade3752f96b2ac
Omar Sakr <o.sakr@ptscorp-eg.com>
2023-01-09 01:35:49 +0200

    Add AOS (animation on scroll)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,21 +1,30 @@
+import { useEffect } from "react";
 import Layout from "../Layout";
 import "../styles/globals.sass";
+
 // Font Awesome
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+
+// Import Animation
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 //React tostify
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-      <ToastContainer />
-    </Layout>
-  );
+	useEffect(() => {
+		AOS.init();
+	}, []);
+	return (
+		<Layout>
+			<Component {...pageProps} />
+			<ToastContainer />
+		</Layout>
+	);
 }
 
 export default MyApp;
